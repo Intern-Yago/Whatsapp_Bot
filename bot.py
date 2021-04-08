e906b1b04a3d7398a93753a19c031d04af382594
Syri Yagowo <yagovictorbotafogo@gmail.com>
2021-04-08 09:52:36 -0300

    change bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import pywhatkit, keyboard, time
 from datetime import datetime
 
-contatos['+556112345678','+556187654321']                                                                                                                               while len(contatos) >= 1:                                                                                                                                                   pywhatkit.sendwhatmsg(teste[0], 'Yago é o melhor e eu sou um bot, não sou ele', datetime.now().hour, datetime.now().second + 2)                                         del contatos[0]
+contatos["+<DDI><DDD><NUMBER>"]#Coloque as respectivas informações aqui                                                                                             
+while len(contatos) >= 1:                                                                                                                                           
+    pywhatkit.sendwhatmsg(teste[0], 'Yago é o melhor e eu sou um bot, não sou ele', datetime.now().hour, datetime.now().second + 2)                                 
+    del contatos[0]
     time.sleep(25)
     keyboard.press_and_release('ctrl + w')
